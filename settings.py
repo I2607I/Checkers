5d2e53c35f8e62c9582d6ee74b9766dd8e7436bf
Igor Polovko <95035997+I2607I@users.noreply.github.com>
2023-04-23 20:38:49 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,20 @@
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+VIOLET = (139, 0, 255)
+DARK_GREEN = (1, 100, 72)
+DARK_YELLOW = (255, 179, 0)
+LIGHT_BROWN = (152, 118, 84)
+CREAMY = (242, 232, 201)
+BLUISH_BLACK = (21, 23, 25)
+GREEN2 = (87, 169, 79)
+
+listStyle = ['Русские', 'Английские', 'Пул Чекерс', 'Международные']
+listColorStyle = [(BLUE, WHITE), (DARK_GREEN, WHITE), (VIOLET, WHITE), (GREEN2, CREAMY)]
+CheckerColor = [(True, BLACK, WHITE), (True, BLUISH_BLACK, CREAMY)]
+CheckerImage = [['orange']]
+listCheckerStyle = CheckerColor + CheckerImage
+listSoundStyle = []
